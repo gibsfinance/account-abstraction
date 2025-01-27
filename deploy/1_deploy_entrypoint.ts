f06e3f95437ec87b09c30e1b9465ee8d513dbb1f
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2025-01-27 15:36:59 -0600

    Refactor deployment scripts for improved readability
    - Format code in deploy/1_deploy_entrypoint.ts
    - Format code in deploy/2_deploy_SimpleAccountFactory.ts
    - Adjust function parameters and indentation for better clarity

--- deploy/1_deploy_entrypoint.ts
+++ deploy/1_deploy_entrypoint.ts
@@ -2,18 +2,19 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { ethers } from 'hardhat'
 
-const deployEntryPoint: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployEntryPoint: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment,
+) {
   const provider = ethers.provider
   const from = await provider.getSigner().getAddress()
 
-  const ret = await hre.deployments.deploy(
-    'EntryPoint', {
-      from,
-      args: [],
-      gasLimit: 6e6,
-      deterministicDeployment: process.env.SALT ?? true,
-      log: true
-    })
+  const ret = await hre.deployments.deploy('EntryPoint', {
+    from,
+    args: [],
+    gasLimit: 6e6,
+    deterministicDeployment: process.env.SALT ?? true,
+    log: true,
+  })
   console.log('==entrypoint addr=', ret.address)
 }
 
